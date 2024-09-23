498d4fa75541279b778d5c84348a427a6785f7f7
Avril Moreno <159593850+AvrilitaMT@users.noreply.github.com>
2024-09-22 23:23:27 -0700

    22/09/2024

--- BasedeDatosWeb/parcial1/Practica 4/practica4.php
+++ BasedeDatosWeb/parcial1/Practica 4/practica4.php
@@ -6,60 +6,58 @@
     $pregunta3 = $_POST["pregunta3"];
     $pregunta4 = $_POST["pregunta4"];
     $pregunta5 = $_POST["pregunta5"];
-    $pregunta6 = $_POST["pregunta6"];
-    $pregunta7 = $_POST["pregunta7"];
-    $pregunta8 = $_POST["pregunta8"];
-    $pregunta9 = $_POST["pregunta9"];
-    $pregunta10 = $_POST["pregunta10"];
 
-
-    //Validar pregunta 1. Cual es tu pareja favorita? 
-    echo "<h3>Cual es tu pareja favorita de grey's?</h3>";
+    //valida pregunta 1 1. ¿Lenguaje de programación que manipula direcciones arbitarias?
+    echo "<h3> ¿Lenguaje de programación que manipula direcciones arbitarias? </h3>";
     echo "<h5>Repuesta seleccionada ".$pregunta1." ---- Correcta = Derek y Meredith</h5>";
-    $p1_respuestas_correctas = 0;
+    echo "<h4>Calificacion EXAMEN </h4>";
+    if($pregunta1 == "derekymeredith"){
+        $aciertos++;
+        echo "<img src='tailsthumbup.jpg' width='100px'><hr>";
+    } else {
+        echo "<img src='tails.jpg' width='100px'><hr>";
+    }
 
-    if(count($pregunta1) == 1){
-        for($i=0; $i<count($pregunta1); $i++){
-            if($pregunta1[$i] == "Derek y Meredith"){
-                $p1_respuestas_correctas ++;
-            }
-        }
-        if($p1_respuestas_correctas == 1){
-            $aciertos++;
-            echo "<img src='goodans' width='50px'><hr>";
-        } else {
-            echo "<img src='wrongans' width='50px'><hr>";
-        } else {
-        echo "<img src='iwringans' width='50px'><hr>"; }
+    //Valido pregunta 2. es una plataforma informática de servicios de transferencia de archivos informaticos basada en internet de Bending Spoon fundada en 2009.
+    echo "<h3>2. _______ es la especialidad de Derek Shepard.</h3>";
+    echo "<h5>Respuesta escrita ".$pregunta2." ------ Correcta = wetransfer</h5>";
+    if(strtolower($pregunta2) == "neurologia" || strtoupper($pregunta2) == "NEUROLOGIA"){
+        $aciertos++;
+        echo "<img src='tailsthumbup.jpg' width='100px'><hr>";
+    } else {
+        echo "<img src='tails.jpg' width='100px'><hr>";
+    }
+
+    //Pregunta 3 - se deben validar 2 opciones html y css
+    echo "<h3> ¿Cual personaje esta vivo? </h3>";
+    echo "<h5>Repuesta seleccionada ".$pregunta3." ---- Correcta = Alex Karev</h5>";
+    echo "<h4>Calificacion EXAMEN </h4>";
+    if($pregunta3 == "alexkarev"){
+        $aciertos++;
+        echo "<img src='tailsthumbup.jpg' width='100px'><hr>";
+    } else {
+        echo "<img src='tails.jpg' width='100px'><hr>";
+    }
+
+    echo "<h3>2. _______ es la especialidad de Derek Shepard.</h3>";
+    echo "<h5>Respuesta escrita ".$pregunta4." ------ Correcta = cirugia plastica</h5>";
+    if(strtolower($pregunta4) == "cirugia plastica" || strtoupper($pregunta4) == "CRIUGIA PLASTICA"){
+        $aciertos++;
+        echo "<img src='tailsthumbup.jpg' width='100px'><hr>";
+    } else {
+        echo "<img src='tails.jpg' width='100px'><hr>";
+    }
+
+    echo "<h3> ¿Que personaje es el mas odiado? </h3>";
+    echo "<h5>Repuesta seleccionada ".$pregunta5." ---- Correcta = Owen Hunt</h5>";
+    echo "<h4>Calificacion EXAMEN </h4>";
+    if($pregunta5 == "owenhunt"){
+        $aciertos++;
+        echo "<img src='tailsthumbup.jpg' width='100px'><hr>";
+    } else {
+        echo "<img src='tails.jpg' width='100px'><hr>";
     }
     
-    echo "<h3>_________ es la especialidad de Derek Shepard.</h3>";
-    echo "<h5>Repuesta seleccionada ".$pregunta2." ---- Correcta = Neurologia</h5>";
-    if(strtoupper($pregunta2) == "neurologia"){
-        $aciertos++;
-        echo "<img src='goodans' width='50px'><hr>";
-    } else {
-        echo "<img src='wrongans' width='50px'><hr>";
-    }
-
-    echo "<h3>question </h3>";
-    echo "<h5>Repuesta seleccionada ".$pregunta3." ---- Correcta = Alex Karev</h5>";
-    if($pregunta3 == "Alex Karev"){
-        $aciertos++;
-        echo "<img src='goodans' width='50px'><hr>";
-    } else {
-        echo "<img src='wrongans' width='50px'><hr>";
-    }
-
-    echo "<h3>_________ es la especialidad de Mark Slone.</h3>";
-    echo "<h5>Repuesta seleccionada ".$pregunta4." ---- Correcta = Cirugia plastica</h5>";
-    if(strtoupper($pregunta4) == "cirugia plastica"){
-        $aciertos++;
-        echo "<img src='goodans' width='50px'><hr>";
-    } else {
-        echo "<img src='wrongans' width='50px'><hr>";
-    }
-
-    echo "CALIFICACIÓN FINAl = " . ($aciertos * 10) . "%";
+    echo "CALIFICACIÓN FINAL = " . ($aciertos * 20) . "%";
 
 ?>
